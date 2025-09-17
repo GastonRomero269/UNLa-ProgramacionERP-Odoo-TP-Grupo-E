399a3a72d8259b8068c1cf332e80a9049aa16123
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2025-09-17 12:54:23 -0300

    Actividades Unidad 1: 17-41
    Se han realizado las actividades de la 17-41 de la unidad 1.

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -7,6 +7,9 @@
     'data': [
         'security/real_estate_res_groups.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_menu_item_views.xml',
     ],
     'application': True,
